edc18b1687a54367bb18ef093eb03bbf76197a25
Ricardo De Gouveia <ricardodgv5@gmail.com>
2020-08-09 18:33:52 -0400

    OutCome.R

--- R Programming/Programming Assignment 3 Quiz/outcome.R
+++ R Programming/Programming Assignment 3 Quiz/outcome.R
@@ -3,4 +3,4 @@
 # Reading in data
 outcome <- data.table::fread('outcome-of-care-measures.csv')
 outcome[, (11) := lapply(.SD, as.numeric), .SDcols = (11)]
-outcome[, lapply(.SD, hist, xlab= "Deaths", main = "Hospital 30-Day Death (Mortality) Rates from Heart Attack", col="lightblue"), .SDcols = (11)]
+outcome[, lapply(.SD, hist, xlab= "Deaths", main = "Hospital 30-Day Death (Mortality) Rates from Heart Attack", col="green"), .SDcols = (11)]
